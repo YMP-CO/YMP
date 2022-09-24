cdf58c3e55920a5836f9ac400659057b0a2d6753
YMP <74971259+YMP-CO@users.noreply.github.com>
2022-09-24 14:28:32 +0300

    Update sw.js

--- sw.js
+++ sw.js
@@ -1,3 +1,51 @@
+var cacheName = 'phaser-v1';
+
+var filesToCache = [
+
+  '/',
+
+  '/index.html',
+
+  '/img/logo.png',
+
+  '/img/icon-192.png',
+
+  '/img/icon-256.png',
+
+  '/img/icon-512.png',
+
+  '/js/game.js',
+
+  '/css/style.css',
+
+  'https://cdn.jsdelivr.net/npm/phaser@3.20.1/dist/phaser.min.js'
+
+];
+
+ 
+
+self.addEventListener('install', function(event) {
+
+  console.log('установка sw');
+
+  event.waitUntil(
+
+    caches.open(cacheName).then(function(cache) {
+
+      console.log('sw кеширует файлы');
+
+      return cache.addAll(filesToCache);
+
+    }).catch(function(err) {
+
+      console.log(err);
+
+    })
+
+  );
+
+});
+
 <script>
 
     if ('serviceWorker' in navigator) {
